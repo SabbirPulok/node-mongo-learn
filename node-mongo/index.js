af5ebc9e38bbd51c61ec3e683dc5da56818fd789
Sabbir_Pulok <sabbir.pulak@gmail.com>
2020-05-02 14:17:01 +0600

    node mongo learn phase 1.0

---
+++ node-mongo/index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const user = ["Jahid","Rakib","Sohan","Gatsby","Barfi"];
+
+const dbName = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+const uri = `mongodb+srv://${dbName}:${dbPassword}@cluster0-7jd2h.mongodb.net/test?retryWrites=true&w=majority`;
+let client = new MongoClient(uri, { useNewUrlParser: true });
+
+app.get('/',(req,res)=>{
+    const fruit = {
+        product: "onion",
+        price :220
+    }
+    res.send(fruit);
+});
+
+app.get('/products',(req,res)=>{
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    const limit = req.query.limit;
+    const priceGT =100;
+
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+
+        collection.find({price:{$lt:"500000"}}).limit(parseInt(limit)).toArray((err,documents)=>{
+            if(err)
+            {
+                res.status(500).send({message:err})
+            }
+            else
+            {
+                console.log("Successfully Inserted ");
+                res.send(documents);
+            }
+        });
+        console.log("database connected....");
+        client.close();
+    });
+})
+app.get('/fruits/banana',(req,res)=>{
+    res.send({
+        product:"banana",
+        quantity:12,
+        price:40
+    })
+})
+
+app.get('/users/:id',(req,res)=>{
+    const id = req.params.id;
+    const sortQuery = req.query.sort;
+    console.log(sortQuery);
+    const name = user[id];
+    res.send({id,name});
+})
+
+app.post('/addProduct',(req,res)=>{
+   client = new MongoClient(uri, { useNewUrlParser: true });
+    const product = req.body;
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        collection.insertOne(product,(err,result)=>{
+            if(err)
+            {
+                res.status(500).send({message:err})
+            }
+            else
+            {
+                console.log("Successfully Inserted ");
+                res.send(result.ops[0])
+            }
+        });
+        console.log("database connected....");
+        client.close();
+    });
+})
+const port =process.env.PORT || 3000;
+app.listen(port,()=>console.log("Listening to port ",port));
